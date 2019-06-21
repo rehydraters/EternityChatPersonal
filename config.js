2f715e965ab1b8827dfae735f7dfa51c338e3276
rehydraters <rehydraters@gmail.com>
2019-06-21 11:01:06 -0500

    more

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+
+// @tombombadiddy
+module.exports = {
+    mind: 'smg',
+    power: 'pineapple',
+    reality: 'mercy',
+    soul: 'glass',
+    space: 'quiet',
+    time: 'teach'
+  }
